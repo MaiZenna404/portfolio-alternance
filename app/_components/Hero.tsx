b0720fad1a4855800e85e8a0abbb88706ccb7eaa
Mai <mai.than.222@gmail.com>
2024-08-30 11:08:31 +0200

    Added Next Icons SVG + Short Presentation

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { ReactIcon } from './icons/ReactIcon';
 import { JavaIcon } from './icons/JavaIcon';
 import { CSharpIcon } from './icons/CSharp';
+import { NextIcon } from './icons/NextIcon';
 
 // Var qui permet de surligner les éléments importants
 const Code = ({ className, ...props}: ComponentPropsWithRef<"span">) => {
@@ -19,8 +20,8 @@ export const Hero = () => {
 <div className="flex-[3]">
 
 <h3 className="text-xl font-caption text-secondary pb-4">Hello, I'm Mai <span className="text-3xl">✌️</span></h3>
-<p className="text-base font-caption text-secondary">Student in Computer Sciences, my aim is to become a Frontend developer. For now, I'm still searching what suits me best, but I do love coding in <Code><ReactIcon size={18} className="inline"/> React</Code> and <Code>NextJs</Code> .</p>
-<p className="text-base font-caption text-secondary">Currently, I'm learning <Code><JavaIcon size={18} className="inline"/> Java</Code> and <Code><CSharpIcon size={18} className="inline"/> C#</Code> basis.</p>
+<p className="text-base font-caption text-secondary">Student in Computer Sciences, my aim is to become a Frontend developer.</p> <p className="text-base font-caption text-secondary mt-4"> For now, I'm still searching what suits me best, but I do love coding in <Code><ReactIcon size={18} className="inline"/> React</Code> and <Code> <NextIcon size={18} className="inline"/> NextJs</Code> .</p>
+<p className="text-base font-caption text-secondary mt-4">Currently, I'm learning <Code><JavaIcon size={18} className="inline"/> Java</Code> and <Code><CSharpIcon size={18} className="inline"/> C#</Code> basis.</p>
 </div>
 
 <div className="flex-1">
