7e01adb1677a6f02849455eed502ea6697436fc9
Mai <mai.than.222@gmail.com>
2024-08-31 20:24:15 +0200

    Added Projects on Portfolio

--- app/_components/Dashboard.tsx
+++ app/_components/Dashboard.tsx
@@ -1,18 +1,68 @@
 import { Card } from "@/components/ui/card";
 import { Section } from "./Section";
+import { CircleUserRound, Home, LucideIcon, UtensilsCrossed } from "lucide-react";
+import Link from "next/link";
 
 export const Dashboard = () => {
     return (
-<Section className="flex max-sm:flex-col items-start font-caption">  
-    <Card className="flex-[2] flex flex-col gap-2 ">
-        Projects
-    </Card>
-<div className="flex-1">
-    <Card>
-        About Me
-    </Card>
-    <Card>Work Experience</Card>
-</div>
-</Section>
+        <Section className="flex max-sm:flex-col font-caption gap-4">
+            <div className="flex-[3] w-full">
+                <Card className="w-full flex-[3] flex flex-col gap-3 p-3">
+                    <p className="text-lg font-semibold p-4">Projects</p>
+                    <div className="flex flex-col gap-4">
+                        {PROJECTS_DESC.map((project, index) => (
+                            <ProjectsIcons
+                                key={index}
+                                Logo={project.Logo}
+                                title={project.title}
+                                description={project.description}
+                                url="/"
+                            />
+                        ))}
+                    </div>
+                </Card>
+            </div>
+            <div className="flex-[2] w-full gap-4 h-full flex flex-col">
+                <Card className="p-3 flex-1 text-base font-semibold">
+                    About Me
+                </Card>
+                <Card className="p-3 flex-1 font-semibold">Work Experience</Card>
+            </div>
+        </Section>
+    );
+};
+
+const PROJECTS_DESC = [
+    {
+        Logo: CircleUserRound,
+        title: "This Portfolio",
+        description: "Online Portfolio made in NextJs, React and TailWind",
+    },
+    {
+        Logo: UtensilsCrossed,
+        title: "Maekgeolli Restaurant",
+        description: 'School Project : "Create the web site of a restaurant of your choice"',
+    },
+
+];
+
+type ProjectsIconsProps = {
+    Logo: LucideIcon;
+    title: string;
+    description: string;
+    url: string;
+};
+
+const ProjectsIcons = (props: ProjectsIconsProps) => {
+    return (
+        <Link href={props.url} className="inline-flex items-center gap-3">
+            <span className="bg-accent text-accent-foreground p-4 rounded-sm">
+                <props.Logo />
+            </span>
+            <div>
+                <p className="text-base font-semibold">{props.title}</p>
+                <p className="text-sm text-muted-foreground">{props.description}</p>
+            </div>
+        </Link>
     );
 };
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -17,14 +17,14 @@ export const Hero = () => {
     return (
 
 <Section className ="flex max-sm:flex-col items-start">
-<div className="flex-[3]">
+<div className="flex-[3] w-full">
 
 <h3 className="text-xl font-caption text-secondary pb-4">Hello, I'm Mai <span className="text-3xl">✌️</span></h3>
 <p className="text-base font-caption text-secondary">Student in Computer Sciences, my aim is to become a Frontend developer.</p> <p className="text-base font-caption text-secondary mt-4"> For now, I'm still searching what suits me best, but I do love coding in <Code><ReactIcon size={18} className="inline"/> React</Code> and <Code> <NextIcon size={18} className="inline"/> NextJs</Code> .</p>
 <p className="text-base font-caption text-secondary mt-4">Currently, I'm learning <Code><JavaIcon size={18} className="inline"/> Java</Code> and <Code><CSharpIcon size={18} className="inline"/> C#</Code> basis.</p>
 </div>
 
-<div className="flex-1 m-auto">
+<div className="flex-[1] m-auto">
     <img className="w-full h-auto max-w-xs max-md:w-56 rounded-full ml-12" src="https://media.licdn.com/dms/image/v2/D4E03AQF2OTX4t1HPjA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1702993005021?e=1730332800&v=beta&t=tKeIPYFMpAngVM17eiLEThKZLAmp1kryEWI1nSbGRIQ" alt="My profile pic" />
 </div>
 </Section>
